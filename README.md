3e6d2ec780d20d2e2b7121e58151b0b3d2ae07f3
Adam Bednarski <adam.bednarski@globoforce.com>
2017-04-06 16:41:41 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # amazon-poly-demo
+
+-- first sign up for AWS account
+-- second in AWES create IAM user with access to polly service
+mkdir $HOME/.aws
+cd $HOME/.aws
+vi credentials
+-- enter data in format:
+	[default]
+	aws_access_key_id = xxx
+	aws_secret_access_key = yyy
+cd $HOME
+git clone https://github.com/abednarski79/amazon-poly-demo.git
+cd amazon-poly-demo
+mvn clean install
+cd target
+java -jar java-demo-0.0.1-SNAPSHOT.jar
--- src/main/java/com.amazonaws.demos.polly/PollyDemo.java
+++ src/main/java/com.amazonaws.demos.polly/PollyDemo.java
@@ -17,11 +17,12 @@ public class PollyDemo {
 
     public static void main(String args[]) throws Exception {
         PollyDemo demo = new PollyDemo();
-        InputStream audioStream = demo.getAudioInputStream();
+        String text = "Gallace is an artist who works within the self-imposed confines of a rigorously limited scale and subject matter. She is a painter of small, unpeopled landscapes in which a modest number of elements a house, a barn, a boat; bushes, grass, sky recur with a quietly mesmerising insistence. In focusing on a particularly favoured motif, the idealised form of a windowless white New England cottage, Gallace succeeds in isolating something universally familiar yet utterly mysterious. Though instantly recognisable as a work by Gallace, each individual painting is a unique rumination on stillness and structure.";
+        InputStream audioStream = demo.getAudioInputStream(text);
         demo.storeInputStreamToTemporaryFile(audioStream);
     }
 
-    private InputStream getAudioInputStream() {
+    private InputStream getAudioInputStream(String text) {
         System.out.println("Creating amazon polly client.");
         AmazonPollyClient polly = new AmazonPollyClient(new DefaultAWSCredentialsProviderChain(),
                 new ClientConfiguration());
@@ -34,7 +35,7 @@ public class PollyDemo {
         Voice voice = describeVoicesResult.getVoices().get(0);
         System.out.println("Creating Amazon Polly TTS request.");
         SynthesizeSpeechRequest synthReq =
-                new SynthesizeSpeechRequest().withText("This is amazon polly test application.").withVoiceId(voice.getId())
+                new SynthesizeSpeechRequest().withText(text).withVoiceId(voice.getId())
                         .withOutputFormat(OutputFormat.fromValue("mp3"));
         System.out.println("Requesting Amazon Polly TTS service.");
         SynthesizeSpeechResult synthRes = polly.synthesizeSpeech(synthReq);
@@ -50,14 +51,18 @@ public class PollyDemo {
         System.out.println("Temporary file name: " + tmpFilePath);
         try {
             FileUtils.copyToFile(fileStreamToStore, new File(tmpFilePath));
+            System.out.println("File copied.");
         } catch (IOException cantReadAudioStreamException) {
-            throw new RuntimeException("Can't copy input stream to file: ", cantReadAudioStreamException);
+            System.err.println("Can't copy input stream to file.");
+            cantReadAudioStreamException.printStackTrace();
         } finally {
             if (fileStreamToStore != null) {
                 try {
                     // outputStream.flush();
                     fileStreamToStore.close();
+                    System.out.println("Audio stream closed.");
                 } catch (IOException e) {
+                    System.err.println("Can't close audio stream.");
                     e.printStackTrace();
                 }
             }
